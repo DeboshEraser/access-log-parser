d5dbdefd72a87c409f89558b541dfc797dab3cb5
Vitaliy Kangin <debosheraser@gmail.com>
2024-03-28 13:49:13 +0400

    Изменили метод main: Убрали определение количества символов строки, введеной пользователем через консоль; Добавили операции сложения, вычитания, умножения и деления двух чисел, вводимых через консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+    public static void main(String[] args)
+    {
+        ckeckFile();
+    }
+
+    public static void ckeckFile() {
+        int attempt = 1;
+        boolean fileExists;
+        boolean isDerictory;
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            fileExists = file.exists();
+            isDerictory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println("Путь к файлу указан неверно!,");continue;
+            }else if (isDerictory){
+                System.out.println("Указан путь к папке!");continue;
+            }else if (fileExists){
+                System.out.println("Путь указан верно. Это файл номер №" + attempt);
+                attempt++;
+            }
+        }
     }
 }
